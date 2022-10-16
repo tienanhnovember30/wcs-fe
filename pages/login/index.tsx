51611ee0cfaccf855e6c82f22c2c2aeb40829b27
Nguyen manh <nguyentienmanhhotboy@gmail.com>
2022-10-16 15:32:55 +0700

    fix

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -15,9 +15,9 @@ const login = (props: Props) => {
                     <div
                         className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
                     >
-                        <Image
-                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                            className="w-full"
+                        <Image layout='fill'
+                            src="/img"
+                            className="w-full" 
                             alt="Sample image"
                         />
                     </div>
