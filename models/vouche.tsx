2aaca456051c19900c9338b73c998a7b4ee8c2ba
Nguyen manh <nguyentienmanhhotboy@gmail.com>
2022-10-20 21:50:15 +0700

    feat vouche

---
+++ models/vouche.tsx
@@ -0,0 +1,9 @@
+export interface Tvouche {
+    _id?: string,
+    code: string,
+    status: string
+    // time:number
+}
+export interface userErr {
+    error: "string"
+}
--- redux/rootReducer.tsx
+++ redux/rootReducer.tsx
@@ -1,9 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./auth";
 import userReducer from "./userSlice";
+import voucheReducer from "./voucheSlice";
 const rootReducer = combineReducers({
   auth: authReducer,
   user: userReducer,
+  vouche: voucheReducer
 });
 
 export default rootReducer;
